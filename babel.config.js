126f77a7ebeb923ca57e6c7f8055ab5631f8943e
OrionFerrin <e1316838554@live.uleam.edu.ec>
2026-03-15 12:41:15 -0500

    Update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,8 @@
 module.exports = {
   presets: [
-    '@vue/cli-plugin-babel/preset'
+    ['@vue/cli-plugin-babel/preset', {
+      useBuiltIns: 'usage',
+      corejs: { version: 3, proposals: true }
+    }]
   ]
 }
